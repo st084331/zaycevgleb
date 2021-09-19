70a5f7d12b956b30b60251f44f59c036bda44362
st084331 <71183199+st084331@users.noreply.github.com>
2021-09-19 23:42:53 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+BOT_TOKEN = "c4bead47388bf2ef47c5c8063af14af36e317ce79186726f621f6d08ba5c24b0159c76ba7209f5bc386da"
+
+# Задержка перед ответом бота в секундах.
+RESPONSE_DELAY = 3
+
+# Шанс ответа в процентах (от 1 до 100).
+RESPONSE_CHANCE = 40
